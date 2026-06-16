854d1f3845266a45c1cddd3f5091ead1d30353d7
Koda-be <debievrelionel@gmail.com>
2026-06-16 22:28:02 +0200

    feat: fleshed out controller class and interface

--- src/main/java/gestionBar/Controller/ControlLayer.java
+++ src/main/java/gestionBar/Controller/ControlLayer.java
@@ -2,14 +2,20 @@ package gestionBar.Controller;
 
 import gestionBar.model.entities.Dish;
 import gestionBar.model.entities.Ingredient;
+import gestionBar.model.entities.Product;
 import gestionBar.model.entities.Wine;
 
-import java.util.Vector;
+import java.util.ArrayList;
 
 public interface ControlLayer
 {
-    void ChangeLogin();
-    Vector<Wine> getWines();
-    Vector<Dish> getDishes();
-    Vector<Ingredient> getIngredients();
+    void exit();
+    void changeLogin();
+
+    void createProduct();
+    ArrayList<Wine> readWines();
+    ArrayList<Dish> readDishes();
+    ArrayList<Ingredient> readIngredients();
+    void updateProduct(Product oldProduct);
+    void deleteProduct(Product p);
 }
--- src/main/java/gestionBar/Controller/Controller/Controller.java
+++ src/main/java/gestionBar/Controller/Controller/Controller.java
@@ -1,36 +1,129 @@
 package gestionBar.Controller.Controller;
 
 import gestionBar.Controller.ControlLayer;
-import gestionBar.model.accessors.Loggers.Logger;
+import gestionBar.model.accessors.DALayer;
+import gestionBar.model.accessors.authenticator.Authenticator;
 import gestionBar.model.entities.Dish;
 import gestionBar.model.entities.Ingredient;
+import gestionBar.model.entities.Product;
 import gestionBar.model.entities.Wine;
+import gestionBar.view.ViewLayer;
 
-import java.util.Vector;
+import java.util.ArrayList;
 
 public class Controller implements ControlLayer
 {
-    private Logger logger;
-    public void ChangeLogin()
-    {
+    private Authenticator authenticator;
+    private ViewLayer view;
+    private DALayer model;
+    private String currentUser;
 
+    public Controller(ViewLayer v, DALayer m)
+    {
+        view = v;
+        model = m;
+        currentUser = null;
+
+        authenticator = model.getAuthenticator();
+    }
+
+    public void start()
+    {
+        model.save();
+        String[] credentials;
+
+        while(true)
+        {
+            credentials = view.PromptForLogin();
+
+            if(authenticator.authenticate(credentials[0], credentials[1]))
+            {
+                currentUser = credentials[0];
+                break;
+            }
+
+            if(credentials[0] == null)
+            {
+                view.showInfoMessage("Login cancelled", "Login cancelled");
+                if(currentUser == null)
+                {
+                    view.showInfoMessage("Login cancelled", "quitting");
+                    System.exit(0);
+                }
+
+                break;
+            }
+            else view.showErrorMessage("Login error", "Incorrect username and/or password");
+        }
+
+        model.setupData(currentUser);
+    }
+
+
+    @Override
+    public void exit()
+    {
+        model.save();
+        System.exit(0);
     }
 
     @Override
-    public Vector<Wine> getWines()
+    public void changeLogin()
     {
-        return null;
+        model.save();
+        view.PromptForLogin();
     }
 
     @Override
-    public Vector<Dish> getDishes()
+    public void createProduct()
     {
-        return null;
+        Product product;
+        if((product = view.PromptForNewProduct()) == null)
+        {
+            view.showInfoMessage("Error", "Invalid input");
+            return;
+        }
+
+        model.createProduct(product);
     }
 
     @Override
-    public Vector<Ingredient> getIngredients()
+    public ArrayList<Wine> readWines()
     {
-        return null;
+        return model.readWines();
+    }
+
+    @Override
+    public ArrayList<Dish> readDishes()
+    {
+        return model.readDishes();
+    }
+
+    @Override
+    public ArrayList<Ingredient> readIngredients()
+    {
+        return model.readIngredients();
+    }
+
+    @Override
+    public void updateProduct(Product oldProduct)
+    {
+        Product newProduct;
+
+        if((newProduct = view.PromptForUpdateProduct(oldProduct)) == null)
+        {
+            view.showInfoMessage("Error", "Invalid input");
+            return;
+        }
+
+        model.updateProduct(oldProduct, newProduct);
+        view.displayProducts();
+    }
+
+    @Override
+    public void deleteProduct(Product p)
+    {
+        model.deleteProduct(p);
+        view.displayProducts();
     }
 }
